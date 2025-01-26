9f7793e1d5367f3000fa0f9a165deb0ec416b64c
Brent Matthew Ortizo <ortizo@chapman.edu>
2025-01-26 06:33:28 -0800

    Fixed bug

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['firebase', 'firebase/auth', 'firebase/firestore'],
+      external: ['firebase', 'firebase/app', 'firebase/auth', 'firebase/firestore'],
     },
   },
 });
